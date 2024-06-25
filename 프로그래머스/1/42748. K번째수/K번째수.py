ee68fd97e29344943d4d9b77ab9fe19d61b2c099
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2024-06-25 15:51:50 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        new_arr = array[c[0]-1:c[1]]
+        new_arr.sort()
+        answer.append(new_arr[c[2]-1])
+    return answer
