3d1097f9bd1a6884fa1b56b05615e6c9f61eea27
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2024-07-05 00:44:33 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 43.58 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log( String(input[0]), '+', String(input[1]), '=',
+                String(Number(input[0]) + Number(input[1])));
+});
