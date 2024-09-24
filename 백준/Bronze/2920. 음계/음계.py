34dd95303ebed1d83532e68964416e76f3046363
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2024-09-24 22:29:12 +0900

    [Bronze II] Title: 음계, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+ascending = '1 2 3 4 5 6 7 8'
+descending = '8 7 6 5 4 3 2 1'
+music = input()
+
+if music == ascending:
+    print('ascending')
+elif music == descending:
+    print('descending')
+else:
+    print('mixed')
